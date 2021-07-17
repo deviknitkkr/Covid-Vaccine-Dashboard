7335de0b13cd8415eeedef1905e656adf2104695
deviknitkkr <deviknitkkr3@gmail.com>
2021-07-17 13:39:21 +0530

    initialized the project

---
+++ src/main/java/com/covid/CovidVaccineDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.covid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidVaccineDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidVaccineDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covid/CovidVaccineDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidVaccineDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
